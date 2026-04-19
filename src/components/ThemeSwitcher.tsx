41c5d5ecae0702ba287a2302c5710365208fc398
DD-ROARO19 <daniroaro19@hotmail.com>
2026-04-18 21:15:09 -0700

    better text contrast against bg

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -24,26 +24,28 @@ const themes: { id: themeOptions; label: string }[] = [
     { id: "github-light", label: 'Light' },
     { id: "catppuccin-latte", label: 'Latte' },
     { id: "rose-pine-dawn", label: 'RoseDawn' },
+    { id: "coral-ocean-dark", label: 'DarkCoralOcean' },
 ];
 
 export function ThemeTest(props: { class?: string }) {
     return (
-        <div class={twMerge("flex gap-1 flex-wrap pt-3 [&>div]:flex-1/8 [&>div]:h-10 [&>div]:rounded-sm text-center", props.class)}>
-            <div class="bg-app-base" >base</div>
-            <div class="bg-app-surface" >surf</div>
-            <div class="bg-app-surface-secondary" >surf 2nd</div>
-            <div class="bg-app-sidebar" >side</div>
-            <div class="bg-app-element" >elem</div>
-            <div class="bg-app-active" >act</div>
-            <div class="bg-app-active-secondary" >act 2nd</div>
-            <div class="bg-app-text text-app-element" >text</div>
+        <div class={twMerge(`flex gap-1 flex-wrap pt-3 [&>div]:flex-1/8 [&>div]:h-10 [&>div]:rounded-sm 
+        text-center text-app-text`, props.class)}>
+            <div class="bg-app-base" ><p>base</p></div>
+            <div class="bg-app-surface" ><p>surf</p></div>
+            <div class="bg-app-surface-secondary" ><p>surf 2nd</p></div>
+            <div class="bg-app-sidebar" ><p>side</p></div>
+            <div class="bg-app-element" ><p>elem</p></div>
+            <div class="bg-app-active text-app-base" ><p>act</p></div>
+            <div class="bg-app-active-secondary" ><p>act 2nd</p></div>
+            <div class="bg-app-text text-app-base" ><p>text</p></div>
 
-            <div class="bg-app-muted" >muted</div>
-            <div class="bg-app-property" >prop</div>
-            <div class="bg-app-string" >str</div>
-            <div class="bg-app-number" >num</div>
-            <div class="bg-app-function" >func</div>
-            <div class="bg-app-keyword" >keyw</div>
+            <div class="bg-app-muted" ><p>muted</p></div>
+            <div class="bg-app-property text-app-base" ><p>prop</p></div>
+            <div class="bg-app-string text-app-base" ><p>str</p></div>
+            <div class="bg-app-number text-app-base" ><p>num</p></div>
+            <div class="bg-app-function text-app-base" ><p>func</p></div>
+            <div class="bg-app-keyword text-app-base" ><p>keyw</p></div>
         </div>
     )
 }
@@ -62,7 +64,7 @@ export default function ThemeSwitcher(props: switcherProps) {
                 <For each={themes}>{(t) => (
                     <button type="button"
                         class={`px-3 py-1 rounded transition-colors ${currentTheme() === t.id
-                            ? 'bg-app-active text-white' // Active state
+                            ? 'bg-app-active' // Active state
                             : 'bg-app-base text-app-muted hover:bg-app-element' // Inactive state
                             }`}
                         onClick={() => setTheme(t.id)}
